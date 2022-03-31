1f38f742b74e74200086d8bd94b570f53fd39e21
gochi_AI <yumemi1204@protonmail.com>
2022-03-31 06:25:58 +0000

    8

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-import os,lxml.html,re,sys
+import os,lxml.html,re,sys,glob
+read = open('README.md',mode='w')
+p=[]
+for i in glob.glob('gochiusa.com/af/core_sys/images/main/*'):
+    p.append(i)
+read.write(i)
