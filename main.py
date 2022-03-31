f98f67bfbf49952653028a0143b313ac36c01e2e
gochi_AI <yumemi1204@protonmail.com>
2022-03-31 10:11:13 +0000

    wait

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os,lxml.html,re,sys,glob
 read = open('README.md',mode='w')
 mp3,png=[],[]
-ypass='2021/core_sys/images/main/cont/*'
+ypass='gochiusa.com/af/core_sys/images/main/cont/*'
 for i in glob.glob(f'{ypass}/*.*',recursive=True):
     if i.endswith('.mp3'):mp3.append(f"{i}")
     if i.endswith('.png'):png.append(f"{i}")
-read.write('<https://gochiai.github.io/gochi_AI-2022ja/>\n')
+read.write('<https://gochiai.github.io/gochi_AI-2022ja/gochiusa.com/af/>\n')
 read.write('# mp3\n')
 for mp3 in mp3:read.write(f'<https://gochiai.github.io/gochi_AI-2022ja/{mp3}>\n')
 char=['chino','cocoa','rize','maya','megu','syaro','chiya','all','fuyu','aoyama','natsume','elu']
